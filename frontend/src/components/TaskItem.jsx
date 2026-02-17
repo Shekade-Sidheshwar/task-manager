1d47213220abc1a011ee701dac4b5537d4d049ea
8Disha-poul <dishapoul808@gmail.com>
2026-02-17 16:46:26 +0530

    change by disha

---
+++ frontend/src/components/TaskItem.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function MessageBox() {
+  const [msg, setMsg] = useState("");
+
+  return (
+    <div>
+      <button onClick={() => setMsg("Login successful!")}>
+        Click Me
+      </button>
+
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default MessageBox;
